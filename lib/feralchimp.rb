dcac19dc1d556828027e77748750dbff4849ad70
Jordon Bedwell <envygeeks@gmail.com>
2013-12-20 10:42:50 -0600

    It was fatally flawed, just use `#is_a?`

--- lib/feralchimp.rb
+++ lib/feralchimp.rb
@@ -7,7 +7,7 @@ require "cgi"
 # Allow for export(1).list flow.
 class FeralchimpErrorHash < Hash
   def initialize(hash = nil)
-    self.replace(hash)
+    self.replace(hash) if hash.is_a?(Hash)
     super
   end
 
